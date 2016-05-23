da5d75851d27155d246ebc64e06a92f3aae72f63
namvp_58 <namvp_58@bitbucket.org>
2016-05-23 12:42:51 +0700

    fix spiner crush

---
+++ app/src/main/java/com/m3t/myapp/MainActivity.java
@@ -0,0 +1,103 @@
+package com.m3t.myapp;
+
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowInsets;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.spnStartPoint)
+    Spinner spnStartPoint;
+    @BindView(R.id.spnEndPoint)
+    Spinner spnEndPoint;
+    @BindView(R.id.etDate)
+    EditText etDate;
+    @BindView(R.id.etTime)
+    EditText etTime;
+    @BindView(R.id.etStop)
+    EditText etStop;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this.getApplicationContext(), R.array.pointArray, R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(R.layout.simple_spinner_dropdown_item);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this.getApplicationContext(), R.array.pointArray, R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(R.layout.simple_spinner_dropdown_item);
+        spnStartPoint.setAdapter(adapter1);
+        spnEndPoint.setAdapter(adapter2);
+    }
+
+    @OnClick({R.id.etDate, R.id.etTime})
+    public void onClick(View view) {
+        final Calendar c = Calendar.getInstance();
+        switch (view.getId()) {
+            case R.id.etDate:
+                int mYear = c.get(Calendar.YEAR);
+                int mMonth = c.get(Calendar.MONTH);
+                int mDay = c.get(Calendar.DAY_OF_MONTH);
+
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                        new DatePickerDialog.OnDateSetListener() {
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+
+                                etDate.setText(dayOfMonth + " tháng " + (monthOfYear + 1) + " " + year);
+
+                            }
+                        }, mYear, mMonth, mDay);
+                datePickerDialog.show();
+                break;
+            case R.id.etTime:
+
+                int mHour = c.get(Calendar.HOUR_OF_DAY);
+                int mMinute = c.get(Calendar.MINUTE);
+
+                // Launch Time Picker Dialog
+                TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                        new TimePickerDialog.OnTimeSetListener() {
+
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay,
+                                                  int minute) {
+                                String AM_PM;
+                                if (hourOfDay < 12) {
+                                    AM_PM = "Sáng";
+                                } else {
+                                    AM_PM = "Chiều";
+                                    if (hourOfDay != 12) hourOfDay -= 12;
+                                }
+                                etTime.setText(hourOfDay + ":" + minute + " " + AM_PM);
+                            }
+                        }, mHour, mMinute, false);
+                timePickerDialog.show();
+                TimePicker timePicker = new TimePicker(this.getApplicationContext());
+                timePicker.setOnApplyWindowInsetsListener(new View.OnApplyWindowInsetsListener() {
+                    @Override
+                    public WindowInsets onApplyWindowInsets(View v, WindowInsets insets) {
+                        return null;
+                    }
+                });
+                break;
+        }
+    }
+}
