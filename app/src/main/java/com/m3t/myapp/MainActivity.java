af1b80d00523653e4a88455cae32f2a6066f637e
namvp_58 <namvp_58@bitbucket.org>
2016-05-26 05:00:53 +0700

    add main activity and main nav drawer

---
+++ app/src/main/java/com/m3t/myapp/MainActivity.java
@@ -0,0 +1,16 @@
+package com.m3t.myapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Nam on 5/26/2016.
+ */
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
