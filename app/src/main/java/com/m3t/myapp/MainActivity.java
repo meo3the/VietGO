b7b4863cd5cff2cf247b22128df016a6ddb8b6ee
meo3the <namtuocnam@gmail.com>
2016-05-27 14:51:50 +0700

    Add drawer layout

--- app/src/main/java/com/m3t/myapp/MainActivity.java
+++ app/src/main/java/com/m3t/myapp/MainActivity.java
@@ -2,8 +2,12 @@ package com.m3t.myapp;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -22,18 +26,29 @@ public class MainActivity extends AppCompatActivity {
     ViewPager container;
     @BindView(R.id.tab)
     TabLayout tab;
-    private SectionalPagerAdapter mpagerAdapter;
+    @BindView(R.id.appBar)
+    AppBarLayout appBar;
+    @BindView(R.id.nav_view)
+    NavigationView navView;
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawerLayout;
+    private SectionalPagerAdapter mPagerAdapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-
+        //setup toolbar
         setSupportActionBar(toolBar);
-
-        mpagerAdapter = new SectionalPagerAdapter(getSupportFragmentManager());
-        container.setAdapter(mpagerAdapter);
+        //set up drawer layout
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolBar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
+        //set up tab layout
+        mPagerAdapter = new SectionalPagerAdapter(getSupportFragmentManager());
+        container.setAdapter(mPagerAdapter);
         tab.setupWithViewPager(container);
     }
 }
